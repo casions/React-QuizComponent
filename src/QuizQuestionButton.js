0a7e3228dce52b14c53be4b635fc39efcec62b34
Stuart Nichols <stuartn@thomasdev.co.uk>
2019-06-17 14:02:49 +0100

    Add QuizQuestionButton

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Quiz from './Quiz';
+
+class QuizQuestionButton extends Component {
+    render() {
+        return (
+        <li>
+            <button>
+                {this.props.button_text}
+            </button>
+        </li>
+        )
+    }
+}
+
+export default QuizQuestionButton
